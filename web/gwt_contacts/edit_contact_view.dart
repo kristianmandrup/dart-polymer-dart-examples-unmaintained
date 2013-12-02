f32a8db36c2fecd7375477ae4049a07a9c9d4c25
Gabor Szabo <szgabsz91@gmail.com>
2013-12-02 11:06:09 +0100

    Change order in edit_contact_view.dart - attributes, events, constructor, enteredView, methods

--- web/gwt_contacts/edit_contact_view.dart
+++ web/gwt_contacts/edit_contact_view.dart
@@ -10,18 +10,18 @@ class EditContactView extends PolymerElement {
   @published Contact contact;
   Contact _originalContact;
   
+  static const EventStreamProvider<CustomEvent> _EDIT_READY_EVENT = const EventStreamProvider("edit-ready");
+  Stream<CustomEvent> get onEditReady => _EDIT_READY_EVENT.forTarget(this);
+  static void _dispatchEditReadyEvent(Element element, bool canceled) {
+    element.dispatchEvent(new CustomEvent("edit-ready", detail: canceled));
+  }
+  
   EditContactView.created() : super.created();
   
   @override
   void enteredView() {
     super.enteredView();
     _originalContact = new Contact.copy(contact);
-  }
-  
-  static const EventStreamProvider<CustomEvent> _EDIT_READY_EVENT = const EventStreamProvider("edit-ready");
-  Stream<CustomEvent> get onEditReady => _EDIT_READY_EVENT.forTarget(this);
-  static void _dispatchEditReadyEvent(Element element, bool canceled) {
-    element.dispatchEvent(new CustomEvent("edit-ready", detail: canceled));
   }
   
   void save() {
